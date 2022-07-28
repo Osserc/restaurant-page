a2304ab087c736d0d9d34932b2e2867d62ba4e67
Stefano Rondelli <rstefanofil@hotmail.com>
2022-07-28 13:45:55 +0200

    Change image output directory, import images

---
+++ src/js/dishes.js
@@ -0,0 +1,6 @@
+import dishRoast from '../images/dishRoast.jpg';
+import dishSkewersNChips from '../images/dishSkewersNChips.jpg';
+import dishSlices from '../images/dishSlices.jpg';
+import dishSteakNTaters from '../images/dishSteakNTaters.jpg';
+import dishTomahawk from '../images/dishTomahawk.jpg';
+
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,15 @@ module.exports = {
             loader: 'sass-loader'
           }
         ]
-      }
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: `images/[name][ext]`
+        }
+      },
     ]
   },
 }
