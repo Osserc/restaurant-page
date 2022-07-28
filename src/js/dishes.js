0168f9c63dbb9b17a78413f91fe3ab3003674640
Stefano Rondelli <rstefanofil@hotmail.com>
2022-07-28 17:48:05 +0200

    Add live preview, tweak dishes description

--- src/js/dishes.js
+++ src/js/dishes.js
@@ -19,11 +19,11 @@ const dishFactory = (title, image, description, price) => {
     return { title, image, description, price }
 }
 
-const dishRoast = dishFactory("Roast", dishRoastImageLoaded, "A mouth-watering roast that will leave your jaw hanging until you sink your teeth in its delicious juiciness.", 35)
+const dishRoast = dishFactory("Roast", dishRoastImageLoaded, "A mouth-watering roast that will make you sink your teeth in its delicious juiciness.", 35)
 const dishSkewersNChips = dishFactory("Skewers with chips", dishSkewersNChipsImageLoaded, "Easy preys for the hurrying customers, with practical chips on the side.", 14)
-const dishSlices = dishFactory("Slices of beef", dishSlicesImageLoaded, "Sometimes, some sliced beef is all you need. Our chefs will cook it to your liking without fail.", 21)
+const dishSlices = dishFactory("Slices of beef", dishSlicesImageLoaded, "Sometimes, some sliced beef is all you need. Do dig in.", 21)
 const dishSteakNTaters = dishFactory("Steak 'n' taters", dishSteakNTatersImageLoaded, "A nice steak and some potatoes make for a perfect meal for a good luncheon.", 28)
-const dishTomahawk = dishFactory("Tomahawk", dishTomahawkImageLoaded, "A juicy and delicious steak for the discerning customer offered with our special sauce.", 28)
+const dishTomahawk = dishFactory("Tomahawk", dishTomahawkImageLoaded, "A juicy and delicious steak for the discerning customer.", 28)
 const allDishes = [dishRoast, dishSkewersNChips, dishSlices, dishSteakNTaters, dishTomahawk]
 
 export { allDishes }
